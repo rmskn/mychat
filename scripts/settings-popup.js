bf7f0c34eee1258035ecff5bf0870c1dcd17f298
Kirill Tremaskin <ktremaskin@codemastersintl.com>
2022-10-26 11:13:57 +0300

    settings popup

---
+++ scripts/settings-popup.js
@@ -0,0 +1,5 @@
+document.getElementById("settings-button")
+    .addEventListener("click", function () {
+        let block = document.getElementById("settings-popup");
+        block.hidden = !block.hidden;
+    });
